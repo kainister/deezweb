ad5df6aafa92e46d6171e31ca15d45232f8cb2da
Kevin Badinca <kevin.badinca@supinternet.fr>
2019-02-01 22:56:52 +0100

    Added style

--- src/components/CardMusic.vue
+++ src/components/CardMusic.vue
@@ -2,14 +2,14 @@
     <article>
         <div>
             <img :src="music.artist.picture" alt="image de la musique">
-            <p>Titre :  {{music.title}}</p><br>
+            <p>Titre :  {{music.title}}</p>
             <p>Artiste :{{music.artist.name}}</p>
         </div>
         <div>
             <audio :src="music.preview" controls></audio>
             <br>
-            <div class="fav">
-                <p>Ajouter aux favoris</p>
+            <div>
+                <button class="fav" @click="addToFav(music.key)">Ajouter aux favoris</button>
             </div>
         </div>
     </article>
@@ -18,14 +18,31 @@
 <script>
 export default {
     name: 'CardMusic',
+    data() {
+        return {
+            favMusic: []
+        }
+    },
     props: {
-        music: Object
-    }
+        music: Object,
+    },
+    methods: {
+        addToFav(key) {
+            this.favMusic.push(this.music[key]);
+            localStorage.setItem('favoriteSong', JSON.stringify(this.favMusic));
+        }
+    },
 }
 </script>
 
 <style>
+article {
+    margin: 20px 0;
+}
 .fav {
-   border: solid red 1px;
+   background-color: red;
+   color: white;
+   border: none;
+   border-radius: 5%; 
 }
 </style>
